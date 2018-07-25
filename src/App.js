08729574ee96c1208dd0252b08337f9c4673704e
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-07-25 13:55:10 +0200

    Add function responsible for fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import fetchData from './app/APIConnector/fetchData'
+
 
 class App extends Component {
+  state = {
+    apiRes: 'hehe'
+  }
+  async componentWillMount() {
+    const res = await fetchData();
+    this.setState({
+      apiRes: res
+    })
+  }
+
   render() {
+    const { apiRes } = this.state;
+
     return (
       <div>
-
+        {apiRes.hasOwnProperty('RPN_data') ? apiRes.RPN_data[0].task : apiRes}
       </div>
     );
   }
